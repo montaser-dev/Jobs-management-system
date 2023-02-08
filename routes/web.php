bd0ff8fe6c658a47e1ca89cc402154920331738e
montaser-dev <montaserdev01@gmail.com>
2023-02-08 19:44:40 +0100

    Creating a Layout & Sections

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 // ALl Listing
 Route::get('/', function () {
     return view('listings', [
@@ -25,8 +13,8 @@ Route::get('/', function () {
 });
 
 // Single Listing
-Route::get('/listings/{id}', function($id){
+Route::get('/listings/{listing}', function(Listing $listing){
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
